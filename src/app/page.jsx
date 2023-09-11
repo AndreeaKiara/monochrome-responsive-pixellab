f27ed86f64afc5a454597407cc9773850b8646ce
AndreeaKiara <69343737+AndreeaKiara@users.noreply.github.com>
2023-09-11 17:10:23 +0300

    base setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="home">
+    <div className="home container">
       <header className="header">Hello</header>
+      <button className="button button-primary">view all</button>
     </div>
   );
 }
---
+++ src/app/sg/page.jsx
@@ -0,0 +1,13 @@
+export default function StyleGuide() {
+  return (
+    <>
+      <div className="mt-4">
+        <button className="button button-primary">view all</button>
+
+        <a href="/" className="button button-primary">
+          Hello
+        </a>
+      </div>
+    </>
+  );
+}
